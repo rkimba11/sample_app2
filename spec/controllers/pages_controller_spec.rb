9e6dadbe5a03362ee5feac53c8bbc35beeadf15d
Ron Kimball <ronkimball@yahoo.com>
2012-04-10 19:28:53 -0600

    Adding test for About page

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -14,6 +14,12 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+    
+  describe "GET 'about'" do
+    it "returns http success" do
+      get 'about'
+      response.should be_success
+    end
   end
 
 end
